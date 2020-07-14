d255a4bd3de49e3bcad71897abdbd8b8129b4d00
Collin <collinziwenwangf@yahoo.com>
2020-07-13 21:43:22 -0400

    Factories

--- src/Factory_Method/FM.java
+++ src/Factory_Method/FM.java
@@ -6,13 +6,15 @@ class Point{
 		this.x = x;
 		this.y = y;
 	}
-	
-	public static Point NewCatesianPoint(double x, double y) {
-		return new Point(x,y);
-	}
-	
-	public static Point NewPolarPoint(double radius, double theta) {
-		return new Point(radius * Math.cos(theta), radius * Math.sin(theta));
+	public static class Factory{
+		public static Point NewCatesianPoint(double x, double y) {
+			return new Point(x,y);
+		}
+		
+		public static Point NewPolarPoint(double radius, double theta) {
+			return new Point(radius * Math.cos(theta), radius * Math.sin(theta));
+		}
+			
 	}
 	
     @Override
@@ -23,7 +25,7 @@ class Point{
 
 class Demo{
 	public static void main(String[] args) {
-		Point p = Point.NewPolarPoint(2, 3);
+		Point p = Point.Factory.NewPolarPoint(2, 3);
 		System.out.println(p);
 	}
 }
