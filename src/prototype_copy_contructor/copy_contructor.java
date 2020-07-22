66079dfa5533771eb2dd7a184f3c32f65a4ea0bc
Ziwen Wang <zi.w.wang@mail.mcgill.ca>
2020-07-21 21:13:15 -0400

    copy contructor

--- src/prototype_copy_contructor/copy_contructor.java
+++ src/prototype_copy_contructor/copy_contructor.java
@@ -1,7 +1,54 @@
 package prototype_copy_contructor;
 
-class Demo{
-	public static void main() {
-		System.out.println("Test");
+
+class Address{
+	public String streetAddress, city, country;
+	public Address(String streetAddress, String city, String country) {
+		this.streetAddress = streetAddress;
+		this.city = city;
+		this.country = country;
 	}
+	
+	public Address(Address other) {
+		this.streetAddress = other.streetAddress;
+		this.city = other.city;
+		this.country = other.country;
+	}
+	
+	@Override
+	public String toString() {
+		return "Address{" + "street address='" + streetAddress + '\'' + ", city='" + city +'\'' +", country='" + country + '\'' +'}'; 
+	}
+}
+
+class Employee{
+	public String name;
+	public Address address;
+	
+	public Employee(String name, Address address) {
+		this.name = name;
+		this.address = address;
+		
+	}
+	
+	public Employee(Employee other) {
+		name = other.name;
+		address = new Address(other.address);
+	}
+	@Override
+	public String toString() {
+		return "Employee{" + "name='" + name + '\'' + ", address='" + address +'}'; 
+	}
+}
+
+class Demo{
+	public static void main(String[] args) {
+		Employee john = new Employee("John", new Address("123 Test Road", "New York", "USA")); 
+		Employee chris = new Employee(john);
+		chris.name = "Chris";
+		chris.address.city = "Montreal";
+		System.out.println(john);
+		System.out.println(chris);
+	}
+	
 }
