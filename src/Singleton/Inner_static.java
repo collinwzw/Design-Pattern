d56fc37c617c28ad1fdb6e0ce86fa33e90aeae18
collinwzw <collinziwenwangf@yahoo.com>
2020-07-28 22:01:53 -0400

    inner singleton

---
+++ src/Singleton/Inner_static.java
@@ -0,0 +1,21 @@
+package Singleton;
+
+
+class InnerStaticSingleton
+{
+	private InnerStaticSingleton() {}
+	
+	private static class Impl
+	{
+		private static final InnerStaticSingleton INSTANCE = new InnerStaticSingleton();
+		
+	}
+	
+	public InnerStaticSingleton getInstance()
+	{
+		return Impl.INSTANCE;
+	}
+}
+
+//thread safety
+//guarantee single object will be initialized
