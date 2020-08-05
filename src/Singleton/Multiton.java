cd00af51a3c77bffa3186e31bacb0dbe6503972f
collinwzw <collinziwenwangf@yahoo.com>
2020-08-04 21:33:31 -0400

    Multiton

--- src/Singleton/Multiton.java
+++ src/Singleton/Multiton.java
@@ -1,3 +1,47 @@
 package Singleton;
 
 import java.util.HashMap;
+
+//restrict to the limited number of creating object
+
+
+enum Subsystem
+{
+	PRIMARY,
+	AUXILIARY,
+	FALLBACK
+}
+
+class Printer
+{
+	private static HashMap<Subsystem, Printer> instances = new HashMap<>();
+	private static int instanceCount = 0;
+	private Printer()
+	{
+		instanceCount++;
+		System.out.println("A total of " + instanceCount + " instances created so far.");
+	}
+	public static Printer get(Subsystem ss)
+	{
+		if (instances.containsKey(ss))
+		{
+			return instances.get(ss);
+		}
+		
+		Printer instance = new Printer();
+		instances.put(ss,instance);
+		return instance;
+	}
+}
+public class Multiton
+{
+	public static void main(String[] args)
+	{
+		Printer main = Printer.get(Subsystem.PRIMARY);
+		Printer aux = Printer.get(Subsystem.AUXILIARY);
+		Printer aux2 = Printer.get(Subsystem.AUXILIARY);
+
+	}
+}
+
+//Lazy loading
